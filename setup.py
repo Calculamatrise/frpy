2b5b2d04ac791d710dda7f615b460829e96a273d
Calculus <51772836+Calculus0972@users.noreply.github.com>
2020-07-01 12:58:16 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import *
+
+with open("README.md", "r") as r:
+    long_description = r.read()
+
+setup(
+    name="frpy", # Replace with your own username
+    version="0.1.0",
+    author="Calculus",
+    author_email="calculus0972@gmail.com",
+    description="An api for FRHD",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Calculus0972/frpy/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    install_requires=[
+        'requests'
+    ],
+    python_requires='>=3.8'
+)
+
+# python3 setup.py sdist bdist_wheel
+# python3 -m twine upload --repository pypi dist/*
